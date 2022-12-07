031ad3ffc8974abaff3fbec2d54c24f5440b1313
mcmfeat <marine.miranda@hotmail.com>
2022-12-07 12:26:04 +0000

    Form css done

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,27 @@
-import React from "react";
-import Form from "./Form";
+import React, { useState } from "react";
+
+import "./Form.css";
 
 export default function Dictionary() {
-  return (<div className="Dictionary">
-    <Form />
-  </div>)
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+        <button>
+          {" "}
+          <i className="fa-solid fa-magnifying-glass" id="search-button"></i>
+        </button>
+      </form>
+    </div>
+  );
 }
