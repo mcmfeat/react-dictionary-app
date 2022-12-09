6d09b975e76ec105bc027282505851984d9f77f4
mcmfeat <marine.miranda@hotmail.com>
2022-12-09 11:51:24 +0000

    Synonyms JSX

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 import "./Meaning.css";
 
 export default function Meaning(props) {
@@ -11,9 +11,14 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
+              <strong>Example:</strong>
               <em> {definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
+      
             </p>
           </div>
         );
