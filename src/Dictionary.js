3862b785119cb9da5bc6355d36b6e61cf871fd64
mcmfeat <marine.miranda@hotmail.com>
2022-12-12 18:41:47 +0000

    Debugging search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,8 @@ import Photos from "./Photos";
 import "./Form.css";
 
 export default function Dictionary(props) {
-  let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState(" ");
+  let [loaded, setLoaded] = useState(false);
   let [results, setResults] = useState(null);
   let [photos, setPhotos] = useState(null);
 
@@ -14,41 +15,54 @@ export default function Dictionary(props) {
     setResults(response.data[0]);
   }
 
-    function handlePexelsResponse(response) {
-      console.log(response.data);
-      setPhotos(response.data.photos);
-    }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleDictionaryResponse);
 
-    const pexelsApiKey = "563492ad6f9170000100000128d5bb38d1cb4263810e07823ef43c24";
+    const pexelsApiKey =
+      "563492ad6f9170000100000128d5bb38d1cb4263810e07823ef43c24";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search} autoComplete="off">
-        <input
-          type="search"
-          placeholder="Type here..."
-          onChange={handleKeywordChange}
-        />
-        <button>
-          <i className="fa-solid fa-magnifying-glass" id="search-button"></i>
-        </button>
-      </form>
-      <Results results={results} />
-      <Photos photos={photos} />
-    </div>
-  );
+
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Type here..."
+            onChange={handleKeywordChange}   
+          />
+          <button>
+            <i className="fa-solid fa-magnifying-glass" id="search-button"></i>
+          </button>
+        </form>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
