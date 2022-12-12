9e3bc0202129b1d1cb3cb7401ca0ed8128449fd1
mcmfeat <marine.miranda@hotmail.com>
2022-12-12 18:33:12 +0000

    Photos component created

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 import "./Form.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
+
+    function handlePexelsResponse(response) {
+      console.log(response.data);
+      setPhotos(response.data.photos);
+    }
 
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey = "563492ad6f9170000100000128d5bb38d1cb4263810e07823ef43c24";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -36,6 +48,7 @@ export default function Dictionary(props) {
         </button>
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} rel="phonetic" target="_blank">
+      <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
         <i class="fa-solid fa-volume-high"></i>
       </a>
       <span className="text">{props.phonetic.text}</span>
